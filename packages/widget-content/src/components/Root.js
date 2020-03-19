18c044c971bb8b55b83b570a7189687df8751656
Tomáš Konrády <tomaskonrady@gmail.com>
2020-03-19 02:13:49 +0100

    senna

--- packages/widget-content/src/components/Root.js
+++ packages/widget-content/src/components/Root.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from '@union-liferay/ui-components';
 
 const Root = ({ data: { messages } }) => (
 	<section>
 		<h1>{messages.heading}</h1>
 		<p>{messages.content}</p>
+		<p>
+			<Link to="/">Back</Link>
+		</p>
 	</section>
 );
 
--- packages/widget-hero/src/components/Root.js
+++ packages/widget-hero/src/components/Root.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from '@union-liferay/ui-components';
 
 import logo from './logo.png';
 
-const Root = () => <img src={logo} />;
+const Root = () => {
+	const [showLink, setShowLink] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => setShowLink(true), 2000);
+	}, []);
+
+	return (
+		<div>
+			<img src={logo} />
+
+			{showLink && (
+				<a href="/web/guest/sub" data-senna-off="true">
+					Normal link
+				</a>
+			)}
+			{showLink && <Link to="/web/guest/sub">Dynamic link</Link>}
+		</div>
+	);
+};
 
 export default Root;
